e9787a53d326ed59a8e257dfc41ec15f92263ced
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2024-05-30 22:11:52 +0700

    Create TCPserver.c

---
+++ TCPserver.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8888
+#define BUFFER_SIZE 1024
+
+int main(void) {
+	int serverFD, clientFD;
+	struct sockaddr_in serverAddr, clientAddr;
+	socklen_t clientLen;
+	pid_t pid;
+
+	serverFD = socket(AF_INET, SOCK_STREAM, 0);//TCP
+	if (serverFD < 0) {
+		printf("%s", "socket() error\n");
+		return 1;
+	}
+
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(PORT);
+
+	if (bind(serverFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+		printf("%s", "bind() error\n");
+		return 1;
+	}
+
+	if (listen(serverFD, 5) < 0) {
+		printf("%s", "listen() error\n");
+		close(serverFD);
+		return 1;
+	}
+
+	while (1) {
+		clientFD = accept(serverFD, (struct sockaddr*)&clientAddr, &clientLen);
+		if (clientFD < 0) {
+			printf("%s", "accept() error\n");
+			continue;
+		}
+
+		pid = fork();
+		if (pid == 0) {
+			char buffer[BUFFER_SIZE];
+			ssize_t numRead;
+
+			while ((numRead = read(clientFD, buffer, BUFFER_SIZE)) > 0) {
+				printf("received: %s\n", buffer);
+				write(clientFD, buffer, numRead);
+			}
+			close(serverFD);
+			return 0;
+		}
+		else {
+			close(clientFD);
+		}
+	}
+
+	return 0;
+}
